a01d2713df0c43ea6609b1dd5cd9ddc2bc5fe871
lbp <43428385+leopolicastro@users.noreply.github.com>
2023-06-19 08:14:19 -0400

    add base function spec

---
+++ spec/dummy/spec/models/func_bot/functions/base_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+require "json"
+
+RSpec.describe FuncBot::Functions::Base do
+  let(:response) do
+    {
+      "choices" => [
+        {
+          "message" => {
+            "function_call" => {
+              "arguments" => '{"arg1": "value1", "arg2": "value2"}'
+            }
+          }
+        }
+      ]
+    }
+  end
+
+  subject { described_class.new(response) }
+
+  describe "#initialize" do
+    it "assigns the response" do
+      expect(subject.response).to eq(response)
+    end
+  end
+
+  describe "#parsed_response" do
+    it "returns the parsed JSON arguments" do
+      expected_result = {"arg1" => "value1", "arg2" => "value2"}
+      expect(subject.parsed_response).to eq(expected_result)
+    end
+  end
+end
